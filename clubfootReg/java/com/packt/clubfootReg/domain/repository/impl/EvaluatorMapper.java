70928683457280a8fba14b416b6922f449341368
jakedye7 <jacob-dye@uiowa.edu>
2015-02-11 19:17:25 -0600

    Update Evaluator java files

---
+++ clubfootReg/java/com/packt/clubfootReg/domain/repository/impl/EvaluatorMapper.java
@@ -0,0 +1,22 @@
+package com.packt.clubfootReg.domain.repository.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import com.packt.clubfootReg.domain.Evaluator;
+
+public class EvaluatorMapper implements RowMapper<Evaluator> {
+
+	public Object extractData(ResultSet arg0) throws SQLException, DataAccessException {
+		return null;
+	}
+
+	public Evaluator mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Evaluator evaluator = new Evaluator();
+		evaluator.setId(rs.getInt("id"));
+		return evaluator;
+	}
+
+}
