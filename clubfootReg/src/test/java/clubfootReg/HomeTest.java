3f6682a6911fee50166a2ab20b0d14cf34073a53
dakre21 <dakre21@gmail.com>
2015-02-17 23:21:29 -0600

    HomeController JUnit test done
    Tests to see if application loads to correct route

--- clubfootReg/src/test/java/clubfootReg/HomeTest.java
+++ clubfootReg/src/test/java/clubfootReg/HomeTest.java
@@ -10,18 +10,44 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Assert;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
 
-/**
- * @author David
- *
- */
+import com.packt.clubfootReg.controller.HomeController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"classpath*:spring/applicationContext.xml"})
 public class HomeTest {
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
 
+	@InjectMocks
+	private HomeController hc;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+
 	}
 
 	/**
@@ -36,6 +62,8 @@ public class HomeTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(hc).build();
 	}
 
 	/**
@@ -46,8 +74,10 @@ public class HomeTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws Exception{
+		 
+	    mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home"));
+		
 	}
 
 }
