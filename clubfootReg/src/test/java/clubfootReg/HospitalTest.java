c9aaef9d745f5e2a240a542b60be7a95237689c9
dakre21 <dakre21@gmail.com>
2015-02-24 16:38:28 -0600

    Added JUnit test case skeletons (unimplemented)
    - patient
    - visit
    - hospital
    - user

---
+++ clubfootReg/src/test/java/clubfootReg/HospitalTest.java
@@ -0,0 +1,68 @@
+package clubfootReg;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+
+import com.packt.clubfootReg.controller.HomeController;
+import com.packt.clubfootReg.controller.HospitalController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"classpath*:spring/applicationContext.xml"})
+public class HospitalTest {
+
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	private HospitalController hc;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(hc).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws Exception {
+		mockMvc.perform(get("/hospital")).andExpect(status().isOk()).andExpect(view().name("hospital"));
+	}
+
+}
