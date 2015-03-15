9778cb0d898cc9f5d5caf4bfd2a2cec24548962a
dakre21 <dakre21@gmail.com>
2015-03-15 13:28:11 -0500

    Documentation for Interfaces added

--- clubfootReg/java/com/packt/clubfootReg/domain/repository/HospitalRepo.java
+++ clubfootReg/java/com/packt/clubfootReg/domain/repository/HospitalRepo.java
@@ -7,14 +7,19 @@ import javax.sql.DataSource;
 
 import com.packt.clubfootReg.domain.Hospital;
 
+/**
+ * 
+ * @author David
+ * Interface for the Hospital Repository to synchronize data to the database via the following interface methods.
+ */
 public interface HospitalRepo {
 	
 	Hospital getHospital1(int id);
-	void addHospital(Hospital hospital);
-	void setDataSource(DataSource ds);
-	List<Hospital> getHospital(int id);
-	void deleteHospital(int id);
-	void updateHospital(Hospital hospital);
-	int getMaxHospitalID();
-	Map<Integer, String> getAllHospitals();
+	void addHospital(Hospital hospital); // addHospital method to add the model data to the database
+	void setDataSource(DataSource ds); // Sets up the dataSource for the database synch
+	List<Hospital> getHospital(int id); // getHospital method that passes the int value for the id
+	void deleteHospital(int id); // DeleteHospital method which takes the id value 
+	void updateHospital(Hospital hospital); // UpdateHospital method which takes in the hospital object
+	int getMaxHospitalID();	// Method that returns the maxhospitalid
+	Map<Integer, String> getAllHospitals();	// Map object that has a vector of Integer and String values which getsAllHospitals
 }
--- clubfootReg/java/com/packt/clubfootReg/domain/repository/UserRepo.java
+++ clubfootReg/java/com/packt/clubfootReg/domain/repository/UserRepo.java
@@ -7,12 +7,17 @@ import javax.sql.DataSource;
 import com.packt.clubfootReg.domain.Hospital;
 import com.packt.clubfootReg.domain.User;
 
+/**
+ * 
+ * @author David
+ * Interface for the User Repository to synchronize data to the database via the following interface methods.
+ */
 public interface UserRepo {
-	void addUser(User user);
-	void setDataSource(DataSource ds);
-	List<User> getUser(int id);
-	List<User> getAllUsers();
-	void deleteUser(int id);
-	void updateUser(User user);
-	int getMaxPersonID();	
+	void addUser(User user); // addUser method to add the model data to the database
+	void setDataSource(DataSource ds); // Sets up the dataSource for the database synch
+	List<User> getUser(int id);	// List object that has the method getUser and takes in an int id
+	List<User> getAllUsers(); // List object that contains the getAllusers method
+	void deleteUser(int id);	// DeleteUser method which takes the id value 
+	void updateUser(User user);	// UpdateUser method which takes in the user object
+	int getMaxPersonID();	 // Method that returns the maxpersonid
 }
